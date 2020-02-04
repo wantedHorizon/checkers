72d3b473214f3c608b097774291c3c11db191041
Eliran <eliran.amzalag@gmail.com>
2020-02-04 12:30:20 +0200

    Add files via upload

---
+++ precache-manifest.b430432337e52b0ede0a622221b54cca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d16f9d70d2bceafc1a3821b8d35384",
+    "url": "/react-checkers/index.html"
+  },
+  {
+    "revision": "94afc8ef023100f555b6",
+    "url": "/react-checkers/static/css/main.c7b2f90e.chunk.css"
+  },
+  {
+    "revision": "0ed2a46b9c3701ffa83f",
+    "url": "/react-checkers/static/js/2.dec1dfb3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-checkers/static/js/2.dec1dfb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94afc8ef023100f555b6",
+    "url": "/react-checkers/static/js/main.e6eb14de.chunk.js"
+  },
+  {
+    "revision": "ebef2d3e31a90e063bd1",
+    "url": "/react-checkers/static/js/runtime-main.5d204e91.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-checkers/static/media/SEGA.12a3db8b.woff"
+  }
+]);
